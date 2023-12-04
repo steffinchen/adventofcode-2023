27b5b46ac2dfeeec64daf56ee0ddec4cb66b83b8
Steffi Wehrle <steffinchen@gmail.com>
2023-12-04 16:52:09 +0100

    ✨ day 2 part 2

--- src/day2.ts
+++ src/day2.ts
@@ -1,42 +1,74 @@
+import { max, min } from 'lodash';
 import { Day } from './day.type.js';
+
+type Color = 'red' | 'green' | 'blue';
+type Round = { [key in Color]: number };
+type Game = {
+  id: number;
+  rounds?: Round[];
+  maxCubes?: Round;
+  minCubes?: Round;
+};
 
 export class Day2 implements Day {
   part1 = (input: string[]) => {
     const exampleRound = { red: 12, green: 13, blue: 14 };
     return input
-      .map((line) => {
-        return this.getMaxCubesPerColor(line);
-      })
+      .map((line) => this.parse(line))
+      .map((game) => this.getMaxCubesPerColor(game))
       .filter((game) => {
         return (
-          game.maxCubes.red <= exampleRound.red &&
-          game.maxCubes.green <= exampleRound.green &&
-          game.maxCubes.blue <= exampleRound.blue
+          game.maxCubes!.red <= exampleRound.red &&
+          game.maxCubes!.green <= exampleRound.green &&
+          game.maxCubes!.blue <= exampleRound.blue
         );
       })
       .map((game) => Number(game.id))
       .reduce((a, b) => a + b);
   };
 
-  getMaxCubesPerColor = (line: string) => {
-    const rounds = line.split(':')[1].trim().split(';');
-    const maxCubes = rounds
-      .flatMap((round) => round.split(',').map((el) => el.trim()))
+  parse = (line: string): Game => {
+    const rounds = line
+      .split(':')[1]
+      .trim()
+      .split(';')
+      .map((round) =>
+        round
+          .split(',')
+          .map((el) => el.trim())
+          .reduce((acc, cur) => {
+            const [count, color] = cur.split(' ');
+            acc[color as Color] = parseInt(count);
+            return acc;
+          }, {} as Round)
+      );
+    const gameId = Number(line.match(/Game (\d+):/)?.[1]);
+    return { id: gameId, rounds: rounds };
+  };
+
+  getMaxCubesPerColor = (game: Game) => {
+    const maxCubes = game.rounds
+      ?.flatMap((round) => Object.entries(round))
       .reduce((acc, cur) => {
-        const [count, color] = cur.split(' ');
-        if (acc[color] === undefined) {
-          acc[color] = parseInt(count);
+        const [color, count] = cur;
+        if (acc[color as Color] === undefined) {
+          acc[color as Color] = count;
         } else {
-          acc[color] = Math.max(acc[color], parseInt(count));
+          acc[color as Color] = Math.max(acc[color as Color], count);
         }
         return acc;
-      }, {} as Record<string, number>);
-    const gameId = line.match(/Game (\d+):/)?.[1];
-    return { id: gameId!, maxCubes: maxCubes };
+      }, {} as Record<Color, number>);
+    return { ...game, maxCubes: maxCubes };
   };
 
   part2 = (input: string[]) => {
-    return 0;
+    return input
+      .map((line) => this.parse(line))
+      .map((game) => this.getMaxCubesPerColor(game))
+      .map((game) => {
+        return Object.values(game.maxCubes!).reduce((acc, cur) => acc * cur);
+      })
+      .reduce((acc, cur) => acc + cur);
   };
 
   testPart1 = () => {
@@ -53,9 +85,15 @@ export class Day2 implements Day {
   };
 
   testPart2 = () => {
-    const example: string[] = [];
-    const sampleResult = this.part1(example);
-    if (sampleResult !== 42)
+    const example: string[] = [
+      'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+      'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+      'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+      'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+      'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+    ];
+    const sampleResult = this.part2(example);
+    if (sampleResult !== 2286)
       throw new Error(`Test result is not as expected: ${sampleResult}`);
   };
 }
